919616543364614daf1feb2842b747ead66822ab
doumar@mum.edu <doumar@mum.edu>
2018-05-22 17:25:55 -0500

    header html and ts files

--- src/app/shared/layout/header.component.ts
+++ src/app/shared/layout/header.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 import { User, UserService } from '../../core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
-  constructor(
-    private userService: UserService
+  constructor( private router: Router,
+    private userService: UserService 
   ) {}
 
   currentUser: User;
+  logout() {
+    this.userService.purgeAuth();
+    this.router.navigateByUrl('/login');
+  }
 
   ngOnInit() {
     this.userService.currentUser.subscribe(
